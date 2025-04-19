f8a9ee4a95a34a0adb6c33b32858552d2871de84
Supriya7472 <supriyashankar7472@gmail.com>
2025-04-19 20:27:15 +0530

    Creating Response Structure class

---
+++ src/main/java/com/example/Blog_App/util/ResponseStructure.java
@@ -0,0 +1,27 @@
+package com.example.Blog_App.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ResponseStructure<T> {
+    private int status;
+    private String message;
+    private T data;
+
+    public static <T> ResponseStructure<T> create(HttpStatus status, String message, T data){
+        ResponseStructure<T> response=new ResponseStructure<>();
+        response.status=status.value();
+        response.message=message;
+        response.data=data;
+
+        return response;
+    }
+}
