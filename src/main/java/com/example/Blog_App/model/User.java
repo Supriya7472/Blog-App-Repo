612cdfbe65d925c77d8665af68a4d05ebc19ba60
Supriya7472 <supriyashankar7472@gmail.com>
2025-04-19 16:54:01 +0530

    Creating User Entity

---
+++ src/main/java/com/example/Blog_App/model/User.java
@@ -0,0 +1,30 @@
+package com.example.Blog_App.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "user_id")
+    private long userId;
+
+    @Column(name="username")
+    private String userName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "ph-no")
+    private String phNo;
+
+}
