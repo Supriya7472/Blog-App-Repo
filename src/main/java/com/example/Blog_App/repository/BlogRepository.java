6260d4ea9826790020d2aad4bef7b2e512339e22
Supriya7472 <supriyashankar7472@gmail.com>
2025-04-20 07:59:40 +0530

    Added method to fetch author details using blog title

--- src/main/java/com/example/Blog_App/repository/BlogRepository.java
+++ src/main/java/com/example/Blog_App/repository/BlogRepository.java
@@ -4,7 +4,12 @@ import com.example.Blog_App.model.Blog;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface BlogRepository extends JpaRepository<Blog,Long> {
     Page<Blog> findAll(Pageable pageable);
+    List<Blog> findByAuthor_UserName(String name);
 }
