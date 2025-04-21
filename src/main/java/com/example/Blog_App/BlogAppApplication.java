baf3b9d48ff64315b48c3689c94b7c56d3b80ff6
Supriya7472 <supriyashankar7472@gmail.com>
2025-04-21 22:53:30 +0530

    Refactor BlogAppApplication to include necessary configurations

--- src/main/java/com/example/Blog_App/BlogAppApplication.java
+++ src/main/java/com/example/Blog_App/BlogAppApplication.java
@@ -2,8 +2,10 @@ package com.example.Blog_App;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 public class BlogAppApplication {
 
 	public static void main(String[] args) {
