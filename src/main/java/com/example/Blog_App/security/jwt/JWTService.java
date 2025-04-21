f1e2649bf402d46f3d8f7cbb0c02cd23acb927ad
Supriya7472 <supriyashankar7472@gmail.com>
2025-04-21 23:19:13 +0530

    Implement JWTService for token generation, validation, and refresh

---
+++ src/main/java/com/example/Blog_App/security/jwt/JWTService.java
@@ -0,0 +1,39 @@
+package com.example.Blog_App.security.jwt;
+
+import com.example.Blog_App.config.AppEnv;
+import com.example.Blog_App.exception.InvalidJWTException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@AllArgsConstructor
+
+public class JWTService {
+    private final AppEnv env;
+
+    public String generateToken(TokenPayload tokenPayload) {
+        return Jwts.builder()
+                .setClaims(tokenPayload.claims())
+                .setIssuedAt(Date.from(tokenPayload.issuedAt()))
+                .setExpiration(Date.from(tokenPayload.expiration()))
+                .signWith(KeyHolder.getKey(env.getSecurity().getSecret()), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Claims parseToken(String token) {
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(KeyHolder.getKey(env.getSecurity().getSecret()))
+                    .build()
+                    .parseClaimsJws(token).getBody();
+        } catch (JwtException | IllegalArgumentException ex) {
+            throw new InvalidJWTException("Failed to parse the token,Invalid JWT");
+        }
+    }
+}
