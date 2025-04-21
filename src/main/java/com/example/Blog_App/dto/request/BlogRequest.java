ede75ca4952bd8acb98e794592629fe5e3e35cd5
Supriya7472 <supriyashankar7472@gmail.com>
2025-04-21 22:55:20 +0530

    Add BlogRequest DTO for blog creation and update requests

---
+++ src/main/java/com/example/Blog_App/dto/request/BlogRequest.java
@@ -0,0 +1,21 @@
+package com.example.Blog_App.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BlogRequest {
+
+    @NotBlank(message = "Title is required")
+    @Size(min = 5, max = 100, message = "Title must be between 5 and 100 characters")
+    private String title;
+
+    @NotBlank(message = "Content is required")
+    @Size(min = 20, message = "Content must be at least 20 characters long")
+    private String content;
+
+}
+
