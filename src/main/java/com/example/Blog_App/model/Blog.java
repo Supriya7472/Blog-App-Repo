51da2e79e014ee177af5c83ec81f64f2f17dfc04
Supriya7472 <supriyashankar7472@gmail.com>
2025-04-21 22:54:19 +0530

    Update Blog model with necessary fields and annotations

--- src/main/java/com/example/Blog_App/model/Blog.java
+++ src/main/java/com/example/Blog_App/model/Blog.java
@@ -1,14 +1,22 @@
 package com.example.Blog_App.model;
 
 import com.example.Blog_App.BlogStatus;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
 
 @Entity
 @Table(name = "blogs")
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 public class Blog {
 
     @Id
@@ -19,12 +27,26 @@ public class Blog {
     @Column(name = "title")
     private String title;
 
-    @Column(name = "content",columnDefinition = "TEXT")
+    @Column(name = "content")
+    @Lob
     private String content;
 
     @Enumerated(EnumType.STRING)
     private BlogStatus status = BlogStatus.POSTED;
 
+    @CreatedDate
+    @Column(name = "createdAt")
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "lastModifiedAt")
+    private Instant lastModifiedAt;
+
+    @CreatedBy
+    @Column(name = "createdBy")
+    private String createdBy;
+
     @ManyToOne
+    @JsonIgnore
     private User author;
 }
